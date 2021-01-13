2e2d5c8998d6646b0c977b6ec1ef6fdebed34e46
kimjiho-cnu <55077828+kimjiho-cnu@users.noreply.github.com>
2021-01-13 16:54:33 +0900

    전화번호 목록 효율성에서 시간초과 효율성 있게 짜보자

---
+++ Hash/전화번호목록.java
@@ -0,0 +1,18 @@
+package Hash;
+import java.util.Arrays;
+public class 전화번호목록 {
+	public boolean solution(String[] phone_book) {
+		boolean answer = true;
+		Arrays.sort(phone_book);
+
+		for(int i = 0 ; i < phone_book.length ; i++){
+			for(int j=i+1; j < phone_book.length ; j++){
+				if(phone_book[j].contains(phone_book[i])){
+					answer = false;
+					break;
+				}
+			}
+		}
+		return answer;
+	}
+}
