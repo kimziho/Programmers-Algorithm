86b711b819636d93d36ad39e7096fb1a42368506
kimjiho-cnu <55077828+kimjiho-cnu@users.noreply.github.com>
2021-01-16 15:08:33 +0900

    베스트 앨범 문제를 똑바로 읽자

--- Hash/베스트앨범.java
+++ Hash/베스트앨범.java
@@ -3,70 +3,57 @@ import java.util.HashMap;
 import java.util.Arrays;
 
 public class 베스트앨범 {
-	 public int[] solution(String[] genres, int[] plays) {
-	        HashMap<String, Integer> first = new HashMap<String, Integer>();
-	        HashMap<String, Integer> second = new HashMap<String, Integer>();
-	        for(int i = 0; i < genres.length;i++){
-	            if(first.containsKey(genres[i])){
-	                if(plays[first.get(genres[i])] < plays[i]){
-	                    first.put(genres[i], i);
-	                }
-	                    
-	            }else{
-	                first.put(genres[i], i);
-	            }
-	        }
-	        
-	        for(int i = 0; i < genres.length; i++){
-	            if(!first.containsValue(i))
-	                if(second.containsKey(genres[i])){
-	                    if(plays[second.get(genres[i])] < plays[i])
-	                        second.put(genres[i],i);
-	                }else{
-	                    second.put(genres[i],i);
-	                }
-	        }
-	        int len = first.size() + second.size();
-	        
-	        int[] answer = new int[len];
-	        for(int i = 0 ; i < len ; i++){
-	            answer[i] = -1;
-	        }
-	        int[] temp = new int[len];
-	        int k = 0;
-	        for(String key : first.keySet()){
-	            temp[k] = plays[first.get(key)];
-	            k++;
-	        }
-	        for(String key : second.keySet()){
-	            temp[k] = plays[second.get(key)];
-	            k++;
-	        }
-	        Arrays.sort(temp);
-	        k--;
-	        for(int i = 0 ; i < temp.length ; i++){
-	            int check = 0;
-	            for(int j = 0 ; j < plays.length; j++){
-	                if(plays[j] == -1)
-	                    continue;
-	                else{
-	                    if(temp[k] == plays[j]){
-	                    answer[i] = j;
-	                    plays[j] = -1;
-	                    check = 1;
-	                        if(second.containsKey(genres[j])){
-	                            i++;
-	                            answer[i] = second.get(genres[j]);
-	                            plays[second.get(genres[j])] = -1;
-	                            break;
-	                        }
-	                    }    
-	                }
-	            }
-	            k--;
-	            if(check == 0)
-	                i--;
-	        }
-	        return answer;
-	    }
+	public int[] solution(String[] genres, int[] plays) {
+		HashMap<String, Integer> first = new HashMap<String, Integer>();
+		HashMap<String, Integer> second = new HashMap<String, Integer>();
+		HashMap<String, Integer> sum = new HashMap<String, Integer>();
+		HashMap<Integer, String> newsum = new HashMap<Integer, String>();
+		for(int i = 0; i < genres.length;i++){  // 장르별로 첫번째
+			if(first.containsKey(genres[i])){
+				if(plays[first.get(genres[i])] < plays[i]){
+					first.put(genres[i], i);
+				}
+
+			}else{
+				first.put(genres[i], i);
+			}
+			if(sum.containsKey(genres[i])){
+				sum.put(genres[i], sum.get(genres[i])+plays[i]);
+			}else{
+				sum.put(genres[i], plays[i]);
+			}
+		}
+
+		for(int i = 0; i < genres.length; i++){ // 장르별로 두번째
+			if(!first.containsValue(i))
+				if(second.containsKey(genres[i])){
+					if(plays[second.get(genres[i])] < plays[i])
+						second.put(genres[i],i);
+				}else{
+					second.put(genres[i],i);
+				}
+		}
+		int len = first.size() + second.size();
+
+		int[] answer = new int[len];
+		int[] temp = new int[sum.size()];
+
+		int k = 0;
+		for(String key : sum.keySet()){
+			temp[k] = sum.get(key);
+			newsum.put(sum.get(key),key);
+			k++;
+		}
+		Arrays.sort(temp);
+		k=0;
+		for(int i = temp.length-1 ; i >= 0; i-- ){
+			answer[k] = first.get(newsum.get(temp[i]));
+			k++;
+			if(second.containsKey(newsum.get(temp[i]))){
+				answer[k] = second.get(newsum.get(temp[i]));
+				k++;
+			}    
+		}
+		return answer;
+	}
 }
