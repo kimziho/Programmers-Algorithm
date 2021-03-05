c0afbb4b44b674ab3ba8edaf0da8478a6db1eb97
kimjiho-cnu <55077828+kimjiho-cnu@users.noreply.github.com>
2021-03-05 13:58:10 +0900

    디스크 컨트롤러 90점, 2차원 배열 정렬해서 다시 시도 예정

--- Heap/디스크컨트롤러.java
+++ Heap/디스크컨트롤러.java
@@ -4,60 +4,42 @@ import java.util.PriorityQueue;
 
 public class 디스크컨트롤러 {
 	public int solution(int[][] jobs) {
-        int answer = 0;
-        int time = 0;
-        int min = jobs[0][0];
-        int min_i = 0;
-        for(int i = 0 ; i < jobs.length ; i++){
-            if(jobs[i][0] < min){
-                min = jobs[i][0];
-                min_i = i;
-            }
-            if(min == 0)
-                break;
-        }
-         // time += jobs[0][1];	// 시작이 0초가 아닐수도?
-        answer = jobs[min_i][1];
-        time = answer;
-        jobs[min_i][0] = -1;
-        int check = 0;
-        while(true){
-            
-            if(check == jobs.length)
-                break;
-            check = 0;
-            PriorityQueue<Integer> p = new PriorityQueue<>();
-            
-            for(int i = 0 ; i < jobs.length ; i++ ){
-                if(jobs[i][0] == -1)
-                {
-                    check++;
-                    continue;
-                }
-                if(jobs[i][0] <= time){
-                    p.add(jobs[i][1]);
-                }
-            }
-            
-            if(!p.isEmpty()){
-                int a = p.poll();
-                for(int i = 0 ; i < jobs.length ; i++) {
-                	if(jobs[i][0] == -1)
-                		continue;
-                	if(jobs[i][1] == a) {
-                		time += a;
-                		answer += (time - jobs[i][0]);
-                		jobs[i][0] = -1;
-                		break;
-                	}
-                }
-                
-            }else{
-                time++;
-            }
+		int answer = 0;
+		int time = 0;
+		int check = 0;
+		while(true){
 
-        }
-        answer /= jobs.length;   
-        return answer;
+			if(check == jobs.length)   // 종료 조건
+			break;
+
+			PriorityQueue<Integer> p = new PriorityQueue<>();
+			check = 0;
+			for(int i = 0 ; i < jobs.length ; i++){
+				if(jobs[i][0] == -1){
+					check++;
+					continue;
+				}
+				if(time >= jobs[i][0])
+					p.add(jobs[i][1]); 
+			}
+			if(!p.isEmpty()){
+				int a = p.poll();
+				for(int i = 0 ; i < jobs.length ; i++) {
+					if(jobs[i][0] == -1)
+						continue;
+					if(jobs[i][1] == a) {
+						time += a;
+						answer += (time - jobs[i][0]);
+						jobs[i][0] = -1;
+						jobs[i][1] = -1;
+						break;
+					}
+				}
+			}else{
+				time++;
+			}
+		}
+		answer /= jobs.length;
+		return answer;
 	}
 }
