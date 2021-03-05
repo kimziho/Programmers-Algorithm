ee8a11c9dc83a16d216d62977c4aa7fe6352d8b1
kimjiho-cnu <55077828+kimjiho-cnu@users.noreply.github.com>
2021-03-05 15:29:39 +0900

    K번째수 copyOfRange를 사용해 배열을 자름

--- Sort/K번째수.java
+++ Sort/K번째수.java
@@ -2,20 +2,16 @@ package Sort;
 import java.util.Arrays;
 public class K¹øÂ°¼ö {
 	public int[] solution(int[] array, int[][] commands) {
-        int length = commands.length;
-        int[] answer = new int[length];
-        for(int i = 0 ; i < length ; i++){
-            int first = commands[i][0];
-            int second = commands[i][1];
-            int dst = commands[i][2];
-            int[] newarray = new int[second - first +1];
-            for( int j = 0 ; j < newarray.length ; j++){
-                newarray[j] = array[first-1+j];
-            }
-            Arrays.sort(newarray);
-            answer[i] = newarray[dst-1];
+        int[] answer = new int[commands.length];     
+        int a,b,c;   
+        for(int i = 0 ; i < commands.length ; i++){
+            a = commands[i][0];
+            b = commands[i][1];
+            c = commands[i][2];
+            int[] temp = Arrays.copyOfRange(array, a-1, b);
+            Arrays.sort(temp);
+            answer[i] = temp[c-1];
         }
-        
         return answer;
     }
 }
