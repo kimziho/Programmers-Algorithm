96306cae44c9300fcd93ec100c903c522b231d14
kimjiho-cnu <55077828+kimjiho-cnu@users.noreply.github.com>
2021-03-05 16:39:46 +0900

    가장 큰 수 for문을 통해서 해결 하려 했지만 실패, string 정렬이 맞는 방법 인듯 하다

--- Sort/가장큰수.java
+++ Sort/가장큰수.java
@@ -1,21 +1,68 @@
 package Sort;
-import java.util.Arrays;
 public class 가장큰수 {
 	public String solution(int[] numbers) {
-		String answer = "";
-
-		String[] Snumber = new String[numbers.length];
-		for(int i = 0 ; i < numbers.length ; i++){
-			Snumber[i] = Integer.toString(numbers[i]);
-		}
-		Arrays.sort(Snumber, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
-		if(Snumber[0].equals("0"))
-			answer += Snumber[0]; 
-		else{
-			for(int i = 0 ; i < numbers.length; i++){  
-				answer += Snumber[i];
-			}
-		}
-		return answer;
-	}
+        String answer = "";
+        //1. 10으로 나눈 값이 가장 큰값
+        //2. 10으로 나눈 값들이 같다면, 뒤에 큰숫자들이 있는것이 우선
+        
+        int[] seq = new int[numbers.length];
+        int[] div_num = numbers.clone();
+        
+        
+        
+        for(int i = 0 ; i < numbers.length ; i++){
+            while(div_num[i] >= 10)
+                div_num[i] = div_num[i] / 10;
+            if(div_num[i] == 0)
+                div_num[i] = div_num[i] % 10;
+        }
+        
+        for(int i = 0 ; i < numbers.length ; i++){
+            int max = -1;
+            int max_i = -1;
+            for(int j = 0 ; j < numbers.length; j++){
+                if(div_num[j] == -1)
+                    continue;
+                if(div_num[j] > max){
+                    max = div_num[j];
+                    max_i = j;
+                    continue;
+                }
+                else if( div_num[j] == max){
+                    if(numbers[j] % 10 == 0) {   // 200 과 20 같은것들
+                        if(numbers[j] < numbers[max_i]){
+                            max = div_num[j];
+                            max_i = j;
+                        }     
+                        continue;
+                    }
+                    int before = numbers[max_i];
+                    if( (before - ( div_num[max_i] * 100)) > 0 )
+                        before -= ( div_num[max_i] * 100);
+                    else if( (before - ( div_num[max_i] * 10)) > 0 )
+                        before -= ( div_num[max_i] * 10);
+                        
+                    int after = numbers[j];
+                    if( (after - ( div_num[j] * 100)) > 0 )
+                        after -= ( div_num[j] * 100);
+                    else if( (after - ( div_num[j] * 10)) > 0 )
+                        after -= ( div_num[j] * 10);
+                        
+                    if(after > before){
+                        max = div_num[j];
+                        max_i = j;
+                    }
+                }
+            }
+            seq[i] = max_i;
+            div_num[max_i] = -1;
+        }
+        if(numbers[seq[0]] == 0)
+            return "0";
+        
+        for(int i = 0 ; i < seq.length ; i++){
+            answer+= numbers[seq[i]];
+        }
+        return answer;
+    }
 }
