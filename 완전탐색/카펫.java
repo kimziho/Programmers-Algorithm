12279a148f636eed535a079b1cf4f9e0388c2d19
kimjiho-cnu <55077828+kimjiho-cnu@users.noreply.github.com>
2021-03-08 17:31:14 +0900

    카펫 문제를 잘읽고 풀자

--- 완전탐색/카펫.java
+++ 완전탐색/카펫.java
@@ -2,34 +2,32 @@ package ¿ÏÀüÅ½»ö;
 
 public class Ä«Æê {
 	public int[] solution(int brown, int yellow) {
-		int[] answer = { 3, 3 };
-		int sum = brown + yellow;
-		int check = 1;
-		while (true) {
-			if (answer[0] * answer[1] == sum) {
-				break;
-			}
-			answer[0]++;
+		int[] answer = new int[2];
+		int a = 3 ;
+		int b = 3 ;
 
-			while (answer[1] <= answer[0]) { 
-				if (answer[0] * answer[0] < sum)
+		int sum = brown + yellow ;
+		if(a*b == sum){ 
+			answer[0] = a;
+			answer[1] = b;
+			return answer;
+		}
+		int i = 3;
+		while(true){
+			while(a >= i){
+				if(a*i > sum)
 					break;
-
-				if (answer[0] * answer[1] == sum) {
-					int a = (answer[0]*2)+ 2*(answer[1]-2);
-					if( a == brown){
-						check = 0;
-						break; 
+				if(a*i == sum){
+					if((a-2) * (i-2) == yellow) {
+						answer[0] = a;
+						answer[1] = i;
+						return answer;
 					}
 				}
-				answer[1]++;
+				i++;
 			}
-
-			if (check == 0)
-				break;
-			answer[1] = 3;
+			a++;
+			i=3;
 		}
-
-		return answer;
 	}
 }
