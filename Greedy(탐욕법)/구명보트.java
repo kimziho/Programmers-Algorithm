db34da88fe8ff817069aad676a592b145d99e7fc
kimjiho-cnu <55077828+kimjiho-cnu@users.noreply.github.com>
2021-03-09 18:10:25 +0900

    구명보트 저번보다 더 간결하게 코딩을 한 것 같다.

--- Greedy(탐욕법)/구명보트.java
+++ Greedy(탐욕법)/구명보트.java
@@ -2,27 +2,23 @@ package Greedy;
 import java.util.Arrays;
 public class 구명보트 {
 	public int solution(int[] people, int limit) {
-		int answer = people.length;
-		Arrays.sort(people);
-
-		int i ; 
-		int j = people.length-1;
-		for(i=0 ; i < people.length; i++){
-			if(i >= j)
-				break;
-			while(true){
-				if(i == j || j < 0){
-					break;
-				}
-
-				if(people[i]+people[j] <= limit){
-					answer--;
-					j--;
-					break;
-				}
-				j--;
-			}
-		}
-		return answer;
-	}
+        int answer = people.length;
+        Arrays.sort(people);        
+        
+        int i = 0;
+        int j = people.length-1;
+        
+        while(i < j){
+            int sum = people[i] + people[j];
+            if(sum <= limit){
+                answer--;
+                i++;
+                j--;
+            }else{
+                j--;
+            }   
+        }
+        return answer;
+    }
 }
+
